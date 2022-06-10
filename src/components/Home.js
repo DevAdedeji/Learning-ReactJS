35d5fa4d1915924ee0e89087f20e5a61b2f49715
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-10 13:33:18 +0100

    Outputting List in ReactJS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,26 @@
 import {useState} from 'react'
 
 function Home() {
-    
 
-    const [name, setName] = useState("Mario")
-
-    const handleClick = ()=>{
-        setName("Luigi")
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
    
-  return (
-    <div className="home">
-        <h2>HomePage</h2>
-        <p>{name}</p>
-        <button onClick={handleClick}>Click Me</button>
-        
-    </div>
-  )
+    return (
+        <div className="home">
+            {
+            blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+
+                </div>
+            ))
+            }
+        </div>
+    )
 }
 
 export default Home
