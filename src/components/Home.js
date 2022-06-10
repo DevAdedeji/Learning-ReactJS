b29660728342f40f14856b58bb736438e181a914
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-10 13:42:08 +0100

    Props in react

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import BlogList from './BlogList'
 
 function Home() {
 
@@ -10,15 +11,7 @@ function Home() {
    
     return (
         <div className="home">
-            {
-            blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-
-                </div>
-            ))
-            }
+           <BlogList blogs={blogs} title="All Blogs"/>
         </div>
     )
 }
