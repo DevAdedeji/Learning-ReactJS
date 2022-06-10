594465fe2703c34159462aca330016f88caa7f7e
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-10 13:18:25 +0100

    useState hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
+import {useState} from 'react'
+
 function Home() {
-    const handleClick = (e)=>{
-        console.log("Hello Ninjas", e);
+    
+
+    const [name, setName] = useState("Mario")
+
+    const handleClick = ()=>{
+        setName("Luigi")
     }
    
   return (
     <div className="home">
         <h2>HomePage</h2>
+        <p>{name}</p>
         <button onClick={handleClick}>Click Me</button>
         
     </div>
