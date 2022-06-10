41bd138d3aa5b9eb6a2a044719f4eb1afa303a83
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-10 12:40:00 +0100

    Dynamic values in components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Adedeji"
+  const likes = 50
+  const website = "www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{title} component</h1>
+     <p>{likes}</p>
+     <a href={website}>Google site</a>
     </div>
   );
 }
