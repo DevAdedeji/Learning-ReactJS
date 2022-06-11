4b32a84a65af4bd4b3d1d33df9c33eed910b50c7
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-11 11:56:53 +0100

    404 page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { Routes, Route} from 'react-router-dom';
 import Blog from './Blog';
 import Create from './Create';
+import NotFound from './components/NotFound';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
        <Route path='/' element={<Home/>}></Route>
        <Route path='/blogs/:id' element={<Blog/>}></Route>
        <Route path='/create' element={<Create/>}></Route>
+       <Route path='*' element={<NotFound/>}></Route>
      </Routes>
      </div>
     </div>
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+  return (
+    <div>
+        <h2>Sorry</h2>
+        <p>That page can't be found</p>
+        <Link to={'/'}>Back to the homepage....</Link>
+    </div>
+  )
+}
+
+export default NotFound
