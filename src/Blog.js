2ef03b524551f4bb28779fd849572952e498a56d
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-11 11:53:50 +0100

    Added delete feature

--- src/Blog.js
+++ src/Blog.js
@@ -1,9 +1,22 @@
 import { useParams } from "react-router"
 import useFetch from "./useFetch"
+import { useNavigate } from "react-router"
 
 function Blog() {
     const {id} = useParams()
     const {data:blog, error, isPending} = useFetch(`http://localhost:8080/blogs/${id}`)
+    const navigate = useNavigate()
+
+    const deleteBlog = (id) =>{
+        fetch("http://localhost:8080/blogs/"+id, {
+            method: 'DELETE',
+          })
+          .then((res)=>{
+              if(res.ok){
+                navigate('/')
+              }
+          })
+    }
     return (
         <div className='blog-details'>
             {isPending && <div>Loading....</div> }
@@ -14,6 +27,7 @@ function Blog() {
                         <h2>{blog.title}</h2>
                         <p>{blog.author}</p>
                         <p>{blog.body}</p>
+                        <button onClick={()=>{deleteBlog(blog.id)}}>Delete</button>
                     </article>
                 )
             }
